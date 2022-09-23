39c5f6a672d171198dd2c3f9563e77283c6f8703
flyingfishfusealt <46322912+flyingfishfuse@users.noreply.github.com>
2022-09-23 11:30:01 -0400

    updating changes
    adding functions for "first run" and "wait for cat to finish"

--- main_cat_feeder.ino
+++ main_cat_feeder.ino
@@ -2,13 +2,22 @@
 #include <EEPROM.h>
 #include <rdm6300.h>
 #include <SoftwareSerial.h>
+#include <Arduino.h>
+
+// change for debugging output, button is internal
+static bool DEBUG true
+
+// switch to check if its being run for the first time
+bool first_run true
 
 /*
 Open Food Flap by activating servo when cat gets near opening
 */
 // create servo object to control a servo
 Servo food_flap;
-SoftwareSerial SerialRFID = SoftwareSerial(6,8); 	//pins = Pins()
+Rdm6300 rdm6300;
+
+//SoftwareSerial SerialRFID = SoftwareSerial(6,8); 	//pins = Pins()
 
 // variable to store the current servo position
 int current_position = 0;
@@ -40,6 +49,8 @@ PINOUT
 #define CLEARCATREGISTRYPIN 9
 #define CAT_FLAP_SERVO_PIN 8
 #define FLAPOPENBUTTONPIN 7
+#define DEBUG_BUTTON 4
+
 #define RFID_RX_PIN 6
 #define RFID_TX_PIN 5
 
@@ -79,32 +90,18 @@ const int CHECKSUM_SIZE = 2;
 bool rfid_detected;
 
 // used to store an incoming data frame 
-uint8_t rfid_tag_in_device[DATA_TAG_SIZE];
+uint8_t rfid_tag_in_device[BUFFER_SIZE];
 int buffer_index = 0;
 
-// actual tag data
-uint8_t* TAG_DATA;
-
+uint8_t stored_rfid_tag[BUFFER_SIZE];
 // used for reading from EEPROM to compare with tag being read in device
 uint8_t rfid_tag_data[DATA_TAG_SIZE];
 
 // is cat allowed to use the feeder?
 bool cat_allowed = false;
 
-/******************************************************************************
- checking for a buffer overflow
-******************************************************************************/
-bool check_buffer_overflow(int buffer_index){
-	
-		if (buffer_index >= BUFFER_SIZE) { 
-			Serial.println("[-] EROOR: Buffer overflow detected!");
-			buffer_index = 0;
-			return true;
-		}
-		else{
-			return false;
-		}
-}
+bool cat_still_there = false
+
 /******************************************************************************
 Opens the door to the cat food
 ******************************************************************************/
@@ -134,10 +131,16 @@ void close_door(){
 	}
 }
 
+
 ////////////////////////////////
 // EEPROM FUNCTIONS
 ////////////////////////////////
-
+uint8_t convertu32_to_u8(uint32_t number_to_convert){
+	uint8_t *arr;
+	//uint32_t Tx_PP= 0x02F003E7;
+	arr=(uint8_t*) &number_to_convert;
+	return &arr;
+	}
 /******************************************************************************
 SET a cat's RFID tag to the registry
 inputs:
@@ -145,10 +148,11 @@ inputs:
 	uint8_t  | byte array with rfid tag number
 ******************************************************************************/
 void WriteTagToRegistry(int address, uint8_t tag_data[]){//}, int arraySize) {
-	for (int i = 0; i < DATA_TAG_SIZE; i++) {
+	for (int i = 0; i < sizeof(tag_data); i++) {
 		EEPROM.write(address+i, tag_data[i]);
 	}
 }
+
 /******************************************************************************
 READ a cat's RFID tag from the registry
 inputs:
@@ -160,33 +164,39 @@ outputs:             |
 ******************************************************************************/
 void ReadTagFromRegistry(int address){//, uint8_t rfid_tag_data[], int arraySize) {
 	// read from specified adddress all the way until DATA_TAG_SIZE
-	for (int i = 0; i < DATA_TAG_SIZE; i++) {
+	for (int i = 0; i < sizeof(rfid_tag_in_device); i++) {
 		// store in global to validate against
-		rfid_tag_data[i] = (uint8_t) EEPROM.read(address+i);
+		stored_rfid_tag[i] = (uint8_t) EEPROM.read(address+i);
 	}
 }
+
 /******************************************************************************
 Setter for cat registry
+modify this function to implement more cats allowed
 ******************************************************************************/
 void set_cat(uint8_t tag_bytes[]){
 	WriteTagToRegistry(CATREGISTRYSTARTADDRESS,tag_bytes);
 }
+
 /******************************************************************************
 Check if cat in scanner is allowed to access feeder
-sets tag data into global
+Compares tag in device with tag stored in EEPROM
+sets tag data into global stored_rfid_tag
 ******************************************************************************/
 bool check_cat(){
-	// sets rfid_tag_data[] with stored tag number
+	// sets stored_rfid_tag[] with tag number from EEPROM
   	ReadTagFromRegistry(CATREGISTRYSTARTADDRESS);
 	// compare with tag being read in device
-  	if (rfid_tag_data == rfid_tag_in_device){
+	if (rfid_tag_in_device == stored_rfid_tag) {
+		Serial.println("[+] Stored tag matches tag in device");
 		return true;
 	}
-	// the tag being read is not in the list of allowed cats
-	else {
+	if (rfid_tag_in_device != stored_rfid_tag){
+		Serial.println("[+] Stored tag matches tag in device");
 		return false;
 	}
 }
+
 /******************************************************************************
 Clear the registry of all registered cat RFID tags
 ******************************************************************************/
@@ -198,69 +208,48 @@ void clear_cat_registry(){
 }
 
 /******************************************************************************
-Converts a hexadecimal value (encoded as ASCII string) to a numeric value
-******************************************************************************/
-long hexstr_to_value(char *str, unsigned int length) { 
-	char* copy = malloc((sizeof(char) * length) + 1); 
-	memcpy(copy, str, sizeof(char) * length);
-	copy[length] = '\0'; 
-	// the variable "copy" is a copy of the parameter "str". "copy" has an additional '\0' element to make sure that "str" is null-terminated.
-	// strtol converts a null-terminated string to a long value
-	long value = strtol(copy, NULL, 16); 
-	 // clean up 
-	free(copy);
-	return value;
-}
-
-/******************************************************************************
 Extracts the tag data from the rfic chip in the dedtector field
 ******************************************************************************/
 void extract_tag() {
-	// first byte of tag information
-		uint8_t msg_head = rfid_tag_in_device[0];
-		/* data from tag
-		 10 byte => data contains 2byte version + 8byte tag
-		*/
-		uint8_t *msg_data = rfid_tag_in_device + 1; 
-		uint8_t *msg_data_version = msg_data;
-		uint8_t *msg_data_tag = msg_data + 2;
-		uint8_t *msg_checksum = rfid_tag_in_device + 11; // 2 byte
-		//uint8_t msg_tail = buffer[13];
-
-		long tag = hexstr_to_value(msg_data_tag, DATA_TAG_SIZE);
-	
-
-		long checksum = 0;
-		for (int i = 0; i < DATA_SIZE; i+= CHECKSUM_SIZE) {
-			long val = hexstr_to_value(msg_data + i, CHECKSUM_SIZE);
-			checksum ^= val;
+	/* get_new_tag_id returns the tag_id of a "new" near tag,
+	following calls will return 0 as long as the same tag is kept near. */
+    //rfid_tag_in_device = rdm6300.get_new_tag_id();
+	if (DEBUG){
+		if (rfid_tag_in_device != 0){
+    		Serial.println("RFID TAG INFO:");
+	  		Serial.println(rdm6300.get_tag_id(), HEX);
+    	}
+	}
+}
+void wait_for_cat_to_finish(){
+	// TESTING CODE
+	if (DEBUG) {
+		// wait 5 seconds
+	 	delay(5000);
+		Serial.println("[+] DEBUG: pretending cat is finished");
+	}
+	// while the cat is still there
+	while (cat_still_there){
+		// if cat no longer in field
+		if (rdm6300.get_tag_id() == 0){
+			//set trigger to let device know cat is not there
+			cat_still_there = false
 		}
+		else{
+			delay(1000)
+		}
+	}
+}
+void check_first_run(){
+	if first_run == true{
+		// wait 30 seconds for PIR to normalize
+		Serial.println("[+] Waiting 30 seconds for PIR to normalize")
+		delay(30000)
+		first_run = false
+		EEPROM.
+	}
 
 
-		// if in debug mode, print data to OLED/serial
-		//if (DEBUG ==true){
-		Serial.println(" (tag)");
-		for (int i = 0; i < DATA_TAG_SIZE; ++i) {
-			Serial.print(char(msg_data_tag[i]));
-		}
-		Serial.print("Message-Checksum (HEX): ");
-		for (int i = 0; i < CHECKSUM_SIZE; ++i) {
-			Serial.print(char(msg_checksum[i]));
-		}
-		Serial.print("Extracted Tag: ");
-		Serial.println(tag);
-		Serial.print("Extracted Checksum (HEX): ");
-		Serial.print(checksum, HEX);
-    // compare calculated checksum to retrieved checksum
-		if (checksum == hexstr_to_value(msg_checksum, CHECKSUM_SIZE)) {
-      // calculated checksum corresponds to transmitted checksum!
-			Serial.print(" ([+] checksum OK)");
-		} else {
-      // checksums do not match
-			Serial.print(" ([-] checksum NOT OK)");
-	  }
-		TAG_DATA = msg_data_tag;
-		//return tag;
 }
 /******************************************************************************
 SETUP 
@@ -276,16 +265,26 @@ void setup() {
 	food_flap.attach(CAT_FLAP_SERVO_PIN);
 
 
- Serial.begin(9600); 
- SerialRFID.begin(9600);
- SerialRFID.listen(); 
- Serial.println("[+] INIT DONE");
+    Serial.begin(9600); 
+    //SerialRFID.begin(9600);
+    //SerialRFID.listen(); 
+
+    rdm6300.begin(RFID_RX_PIN);
+
+    Serial.println("[+] INIT DONE");
 }
 
 /******************************************************************************
 main loop
 ******************************************************************************/
 void loop() {
+    // read rfid
+    //if (rdm6300._update()){
+		rfid_tag_in_device = convertu32_to_u8(rdm6300.get_tag_id());
+        //rfid_tag_in_device = rdm6300.get_tag_id();
+        // set trigger
+        rfid_detected = true;
+    //}
 	//read button states
 	// TODO: do something stupid if all buttons pressed,
 	// make led flash funny?
@@ -298,91 +297,57 @@ void loop() {
 	BUTTON ACTIONS
 	**************************/
 	// if button to register cat is pressed
-	if (add_to_registry == 1);{
-		set_cat(TAG_DATA);
+	if (rfid_detected && add_to_registry == 1);{
+		set_cat(rfid_tag_in_device);
 	}
 	// if button to change door position is pressed
 	if (actuate_door_button == 1){
 		// door is open, close door
 		if (DOOROPEN == true){
 		close_door();
+        DOOROPEN = false;
 		}
 		// door is closed, open door
 		else if (DOOROPEN == false){
 		open_door();
+        DOOROPEN = true;
 		}
 	}
 
 	/***************************
 	RFID DETECTION AND BEHAVIOR
 	***************************/
-	// if setup ran properly
-	if (SerialRFID.available() > 0){
+    // returns 0 if no tag near, or in second itteration
+    //rfid_tag_in_device = rdm6300.get_new_tag_id()
+    // tag is near device
+	if (rfid_detected){
+        // print data for debugging
+        Serial.println("RFID TAG INFO:");
+		Serial.println(rfid_tag_in_device, HEX);
 
-		// while reading stream from rfid scanner
-		int rfid_stream = SerialRFID.read();
-
-		// if no data was read
-		if (rfid_stream == -1) { 
-			Serial.println("[-] Failed to read: No data received");
-			return;
+		// checks if cat is allowed to use feeder
+		cat_allowed = check_cat();
+		// cat IS allowed to use feeder
+		if (cat_allowed){
+			// open the door
+			open_door();
+			// set trigger to let device know cat is present
+			// cant let it get pinched!
+			cat_still_there = true
+			// wait until the cats rfid tag is no longer in the detection field
+			// of the rfid reader
+			// this function also closes the door
+			wait_for_cat_to_finish();
 		}
-
-		// RDM630/RDM6300 found a tag => tag incoming
-		if (rfid_stream == 2) {
-			buffer_index = 0;
-			rfid_detected = true;
-			} 
+		// cat IS NOT allowed to use feeder
+		// fuck off asshole
+		else{
+			//continue;
+		}
 		
-		// tag has been fully transmitted
-		else if (rfid_stream == 3) {
-			// extract tag at the end of the function call
-			call_extract_tag = true;
-		}
-
-		//if (check_buffer_overflow()){
-		//	return;
-		if (buffer_index >= BUFFER_SIZE) { 
-			Serial.println("[-] EROOR: Buffer overflow detected!");
-			buffer_index = 0;
-		}
-
-		//if (buffer_index >= BUFFER_SIZE) { // checking for a buffer overflow (It's very unlikely that an buffer overflow comes up!)
-		//	Serial.println("Error: Buffer overflow detected! ");
-		//	return;
-		//}
-		// everything is alright => copy current value to buffer
-		rfid_tag_in_device[buffer_index++] = rfid_stream;
-
-		if (rfid_detected == true && call_extract_tag == true) {
-			if (buffer_index == BUFFER_SIZE) {
-				// assigns tag data to global variable
-				extract_tag();
-
-				// check for buffer overflow 
-				if (buffer_index >= BUFFER_SIZE) { 
-					Serial.println("[-] EROOR: Buffer overflow detected!");
-					buffer_index = 0;
-				}
-
-				// checks if cat is allowed to use feeder
-				cat_allowed = check_cat(TAG_DATA);
-				// cat IS allowed to use feeder
-				if (cat_allowed){
-					open_door();
-				}
-				// cat IS NOT allowed to use feeder
-				// fuck off asshole
-				else{
-					//continue;
-				}
-			
-			// something is wrong... start again looking for preamble (value: 2)
-			} else {
-				buffer_index = 0;
-				return;
-			}
-		}		
+	// something is wrong... start again looking for preamble (value: 2)
+	} else {
+		//return;
 	}		
-	// if a cat walked into the device and its rfid tag matched one in the registry
-}
+}	
+
